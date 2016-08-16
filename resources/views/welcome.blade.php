ec4c6d5001d7c3a0d40753755be77bc6357ff8a1
sqrtqiezi <sqrtqiezi@foxmail.com>
2016-08-16 12:53:36 +0800

    init project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Whirlpool</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+<div id="big-video-wrap">
+    <video autoplay muted loop id="bgvid">
+        <source src="http://obz9qz3z4.bkt.clouddn.com/whirlpool" type="video/mp4">
+    </video>
+</div>
+<div class="main wrapper clearfix">
+    <div id="video-raster"></div>
+    <article>
+        <div class="content">
+            <section class="grid-cell">
+                <div class="logo">
+                    <img src="/images/logo.png" alt="">
+                </div>
+            </section>
+            <section class="grid-cell"></section>
+            <section class="grid-cell"></section>
+        </div>
+        <div class="grid">
+            <section class="grid-cell grid-cell-no-1"></section>
+            <section class="grid-cell grid-cell-no-2"></section>
+            <section class="grid-cell grid-cell-no-3"></section>
+            <section class="grid-cell grid-cell-no-4"></section>
+            <section class="grid-cell grid-cell-no-5"></section>
+            <section class="grid-cell grid-cell-no-6"></section>
+            <section class="grid-cell grid-cell-no-7"></section>
+            <section class="grid-cell grid-cell-no-8"></section>
+            <section class="grid-cell grid-cell-no-9"></section>
+            <section class="grid-cell grid-cell-no-10"></section>
+            <section class="grid-cell grid-cell-no-11"></section>
+            <section class="grid-cell grid-cell-no-12"></section>
+            <section class="grid-cell grid-cell-no-13"></section>
+            <section class="grid-cell grid-cell-no-14"></section>
+            <section class="grid-cell grid-cell-no-15"></section>
+            <section class="grid-cell grid-cell-no-16"></section>
+            <section class="grid-cell grid-cell-no-17"></section>
+            <section class="grid-cell grid-cell-no-18"></section>
+            <section class="grid-cell grid-cell-no-19"></section>
+            <section class="grid-cell grid-cell-no-20"></section>
+        </div>
+    </article>
+
+    <div class="footer">
+
+    </div>
+</div>
+
+</body>
+<script src="js/app.js"></script>
+</html>
