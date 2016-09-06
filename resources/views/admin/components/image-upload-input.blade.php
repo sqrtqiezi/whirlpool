79ab1ac013e02858ae738e66b5c5c769d5baaa92
abrahamgreyson <82011220@qq.com>
2016-09-06 18:29:37 +0000

    refactor: 模版微调

---
+++ resources/views/admin/components/image-upload-input.blade.php
@@ -0,0 +1,43 @@
+<div
+		class="form-group {!! $thumbnailError = $errors->has('thumbnail') ? 'has-error' : null !!}" id="fileupload-group">
+	<label for="inputEmail3" class="col-sm-2 control-label">封面图</label>
+
+	<div class="col-sm-10">
+
+		<!-- The fileinput-button span is used to style the file input field as button -->
+		<div class="row">
+			<div class="col-xs-2">
+					<span class="btn btn-primary fileinput-button">
+					<i class="glyphicon glyphicon-plus"></i>
+					<span>上传...</span>
+					<input type="file" name="file" id="fileupload">
+						<input type="hidden" name="thumbnail" value="{!! $terminal->thumbnail or old('thumbnail') !!}" id="thumbnail-input">
+			</span>
+			</div>
+			<div class="col-xs-10">
+				{{-- 进度条 --}}
+				<div class="progress progress-xs" style="margin: 15px 0; display: none;" id="fileupload-progress">
+					<div class="progress-bar progress-bar-primary" style="width: 0"></div>
+				</div>
+				{{-- /.进度条 --}}
+			</div>
+		</div>
+		<div class="row" style="margin-top: 10px;">
+			<div class="col-xs-12">
+				{{-- 预览 --}}
+				<div id="fileupload-preview" style="display:{!! isset($terminal->thumbnail_url) || old('preview_url') ? 'block' : 'none' !!};">
+					<a href="{!! $terminal->thumbnail_url or old('preview_url') !!}" title="">
+						<img src="{!! $terminal->thumbnail_url or old('preview_url') !!}"
+						     style="max-width: 400px;max-height: 400px;">
+					</a>
+					<input type="hidden" name="preview_url" id="preview-url-hidden" value="{!! $terminal->thumbnail_url or old('preview_url') !!}">
+				</div>
+				{{-- /.预览 --}}
+			</div>
+		</div>
+		@if($thumbnailError)
+			<span class="help-block">{!! $errors->first('thumbnail') !!}</span>
+		@endif
+		<span class="help-block" id="ajax-message" style="display: none"></span>
+	</div>
+</div>
---
+++ resources/views/admin/components/image-upload.blade.php
@@ -0,0 +1,71 @@
+@section('style-in-header')
+	@parent
+	<link rel="stylesheet" href="{!! asset('adminlte/css/jquery.fileupload.css') !!}">
+	<link rel="stylesheet" href="{!! asset('adminlte/fancybox/jquery.fancybox.css') !!}">
+@append
+
+@section('script-in-footer')
+	@parent
+	<script src="{!! asset('adminlte/js/jquery.ui.widget.js') !!}"></script>
+	<script src="{!! asset('adminlte/js/jquery.fileupload.js') !!}"></script>
+	<script src="{!! asset('adminlte/fancybox/jquery.fancybox.js') !!}"></script>
+	<script>
+		$(function () {
+			$('#fileupload').fileupload({
+				type: 'POST',
+				url: '{!! route('panel.api.upload') !!}',
+				autoUpload: true,
+				// 进度条
+				progressall: function (e, data) {
+					var progress = parseInt(data.loaded / data.total * 100, 10);
+					$('.progress-bar').css(
+							'width',
+							progress + '%'
+					);
+				},
+				send: function (e, data) {
+					// 显示进度
+					$('#fileupload-progress').show();
+				},
+				always: function (e, data) {
+					// 隐藏进度
+					$('#fileupload-progress').fadeOut(2);
+					var response = data.jqXHR.responseJSON;
+					// 5xx
+				  if(!response) {
+					  alert('服务器错误，请联系管理员');
+				  } else {
+				  	// JSON response
+					  // 成功
+					  if (response.error == 0) {
+					  	// 显示预览
+						  $('#fileupload-preview').show().find('img').attr('src', response.thumbnail);
+						  $('#fileupload-preview').find('a').attr('href', response.thumbnail)
+								  .attr('title', response.origin_name);
+						  // 预览暂存到 request 中
+						  $('#preview-url-hidden').val(response.thumbnail);
+						  // 填充隐藏表单
+						  $('#thumbnail-input').val(response.file);
+					  } else {
+					  	// 错误提示
+						  $('#fileupload-group').addClass('has-error');
+						  $('#ajax-message').text(response.message).show();
+					  }
+				  }
+				}
+			});
+
+			$('#fileupload-preview').popover({html: true});
+			// fancybox
+			$("#fileupload-preview a").fancybox({
+				openEffect	: 'elastic',
+				closeEffect	: 'elastic',
+				helpers : {
+					title : {
+						type : 'inside'
+					}
+				}
+			});
+		});
+	</script>
+@append
