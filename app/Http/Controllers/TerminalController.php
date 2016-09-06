021d27ee7783b3e704f40b977e68b7ed7214ef3b
abrahamgreyson <82011220@qq.com>
2016-09-06 02:58:22 +0000

    refector: 控制器方法 pull up

--- app/Http/Controllers/TerminalController.php
+++ app/Http/Controllers/TerminalController.php
@@ -125,12 +125,4 @@ class TerminalController extends Controller
 
         return redirect()->back();
     }
-
-    /**
-     * 暂存列表页 url，防止更新或新建后，回不到原来的筛选结果
-     */
-    protected function setPreviousUrl()
-    {
-        app('session')->put('url.intended', app('url')->previous());
-    }
 }
