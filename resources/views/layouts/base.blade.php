677b567eca5b5e8986af1660bd7c79d5c9e05d27
sqrtqiezi <sqrtqiezi@foxmail.com>
2016-09-09 11:56:16 +0800

    feat: 手机端页面添加底部banner

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -44,7 +44,7 @@
         </div>
     </article>
     <div class="footer clearfix">
-        <nav class="nav-list pull-left">
+        <nav class="nav-list pull-left only-desktop">
             <ul class="nav-top-list">
                 <li>
                     <a data-pjax href="javascript:;" class="js-top-nav" data-sub-nav="about-sub-nav">
@@ -119,16 +119,17 @@
             </ul>
         </nav>
         <div class="footer-info pull-right">
-            <div class="content-info pull-left">
-                <span class="company-name">惠而浦（中国）股份有限公司</span>
-                <span class="company-right">沪ICP备13004331号 © 2016 Whirlpool .All Right Reserved</span>
-            </div>
             <div class="contact-info pull-right">
                 <a data-pjax href="http://weibo.com/u/5889505040" class="contact-weibo"></a>
                 <a data-pjax href="javascript:;" class="contact-wechat"></a>
                 <a data-pjax href="javascript:;" class="contact-email"></a>
-                <span class="design-info">Design by wisnovo</span>
+                <span class="design-info only-desktop">Design by wisnovo</span>
             </div>
+            <div class="content-info pull-left">
+                <span class="company-name">惠而浦（中国）股份有限公司</span>
+                <span class="company-right">沪ICP备13004331号 © 2016 Whirlpool .All Right Reserved</span>
+            </div>
+
         </div>
     </div>
 </div>
