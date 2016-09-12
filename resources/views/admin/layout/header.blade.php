d7efd7535e3f1591d29fadfe836b716d9a342c3b
sqrtqiezi <sqrtqiezi@foxmail.com>
2016-09-12 10:42:43 +0800

    refactor: 静态资源由cdn改为本地

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -17,10 +17,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
 		      href="{!! asset('adminlte/bootstrap/css/bootstrap.min.css') !!}">
 		<!-- Font Awesome -->
 		<link rel="stylesheet"
-		      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+		      href="{!! asset('adminlte/bootstrap/css/font-awesome.min.css') !!}">
 		<!-- Ionicons -->
 		<link rel="stylesheet"
-		      href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+		      href="{!! asset('adminlte/bootstrap/css/ionicons.min.css') !!}">
 		<!-- Theme style -->
 		<link rel="stylesheet"
 		      href="{!! asset('adminlte/css/AdminLTE.min.css') !!}">
